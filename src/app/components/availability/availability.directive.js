2943557878d527bd401c8e23cac77b6e3567f3ef
Karolis Grinkevicius <k.grinkevicius@gmail.com>
2015-12-04 22:30:32 +0200

    Some minor changes

--- src/app/components/availability/availability.directive.js
+++ src/app/components/availability/availability.directive.js
@@ -16,24 +16,43 @@ class AvailabilityController {
 
     this.q = $q;
     this.AvailabilityService = AvailabilityService;
-    this.setAvailabilityData();
+    this.onAvailabilityDataChange();
     AvailabilityService.findCurrentLot().then( lot => { this.currentLot = lot; } );
     this.loading = new Map;
   }
 
-  setAvailabilityData() {
-    console.log("Setting availability data.");
-    this.AvailabilityService.getAvailability().$promise.then(availability => {
-      let numbers = availability.map(lot => lot.number);
-      this.parkingAvailabilityData = availability.filter((lot,index) => numbers.indexOf(lot.number) === index);
-      numbers.map(number => {this.loading.set(number, false)});
-      console.log(availability);
+  static getAvailabilityLotsNumbers(lots) {
+    return lots.map(lot => lot.number);
+  }
+
+  getAvailabilityData() {
+    var deferred = this.q.defer();
+    if (angular.isArray(this.AvailabilityService)) {
+      deferred.resolve(this.AvailabilityService);
+    } else {
+      this.AvailabilityService.getAvailability().$promise
+        .then(availability => {
+          let numbers = AvailabilityController.getAvailabilityLotsNumbers(availability);
+          this.parkingAvailabilityData = availability.filter((lot,index) => numbers.indexOf(lot.number) === index);
+          deferred.resolve(this.parkingAvailabilityData);
+        })
+        .catch(() => {
+          deferred.reject("Could not retrieve parking lots from the server.");
+        });
+    }
+
+    return deferred.promise;
+  }
+
+  onAvailabilityDataChange() {
+    this.getAvailabilityData().then(availability => {
+      AvailabilityController.getAvailabilityLotsNumbers(availability).map(number => {this.loading.set(number, false)});
     });
   }
 
   fakeReservePromise() {
     var deferred = this.q.defer();
-    setTimeout( () => { deferred.resolve("Resolved...") }, 400 );
+    setTimeout( () => { deferred.resolve("Resolved...") }, 2000 );
 
     return deferred.promise;
   }
@@ -51,21 +70,25 @@ class AvailabilityController {
   }
 
   reserve(lot) {
-    this.setLoading(lot);
-    this.fakeReservePromise().then( _=> {
-      this.resetLoading();
-      this.setAvailabilityData();
-    });
+    if (!this.isLoading(lot)) {
+      this.setLoading(lot);
+      this.fakeReservePromise().then( _=> {
+        this.resetLoading();
+        this.onAvailabilityDataChange();
+      });
+    } else {
+      console.log("STOP! I am loading this..");
+    }
   }
 
-  isfreeUpLotAvailable(lot) {
+  isFreeUpLotAvailable(lot) {
     return !!this.currentLot;
   }
 
   freeUpLot(lot) {
     this.AvailabilityService.freeUpLot(lot)
       .then(response => {
-        this.setAvailabilityData();
+        this.onAvailabilityDataChange();
         console.log(response);
       })
       .catch(response => {
@@ -82,13 +105,23 @@ class AvailabilityController {
     console.log("Searching if reset lot is available...");
     console.log(this.currentLot.number);
     console.log(this.parkingAvailabilityData);
+
+    /*this.parkingAvailabilityData.map(lot => {
+      if (lot.number === this.currentLot.number) {
+        console.log("Found!");
+        return true;
+      }
+    });*/
+
+    console.log(typeof this.parkingAvailabilityData);
+
     return false;
   }
 
   resetLot(lot) {
     this.AvailabilityService.resetLot(lot)
       .then(response => {
-        this.setAvailabilityData();
+        this.onAvailabilityDataChange();
         console.log(response);
       })
       .catch(response => {
