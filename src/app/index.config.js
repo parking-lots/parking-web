85d90445fc9132493ee61f23f1909514f753a92d
Oleg Sviderskij <oleg@sviderskij.lt>
2016-01-20 01:12:40 +0200

    Week starting day monday

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,10 +1,11 @@
-export function config ($logProvider, toastrConfig, $httpProvider, $locationProvider) {
+export function config ($logProvider, toastrConfig, $httpProvider, $locationProvider, datepickerPopupConfig) {
   "ngInject";
   // Enable log
   $logProvider.debugEnabled(true);
   $httpProvider.defaults.withCredentials = true;
 
   $locationProvider.html5Mode(true);
+  datepickerPopupConfig.startingDay = 1;
 
   // Set options third-party lib
   toastrConfig.allowHtml = true;
