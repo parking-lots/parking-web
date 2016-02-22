71860b3c35f5761e072e2dde5c3e4e875cd60186
Karolis Grinkevicius <k.grinkevicius@gmail.com>
2016-02-22 14:48:05 +0200

    [Feature] - do not attempt to reserve lot for same username

--- src/app/components/availability/availability.directive.js
+++ src/app/components/availability/availability.directive.js
@@ -84,7 +84,7 @@ class AvailabilityController {
   }
 
   reserve(lot) {
-    if (!lot.currentlyUsed) {
+    if (!lot.currentlyUsed && lot.user.username !== this.profile.username) {
       this.setLoading(lot);
       this.AvailabilityService.reserveFreeSpot(lot)
         .then( _=> {
