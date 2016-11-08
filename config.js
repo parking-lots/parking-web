057ca852224d837587e7a0db0d0e17406eeaf7c4
Jurgis Bridzius <jurgis@bridzius.lt>
2016-11-08 22:23:22 +0200

    Added shared config for all environments

---
+++ config.js
@@ -0,0 +1,18 @@
+
+var shared = {
+  apiUrl: process.env.API_URL || "https://test.parkinger.net/api"
+};
+
+var environments = {
+  dev: {
+    ENV_VARS: shared
+  },
+  test: {
+    ENV_VARS: shared
+  },
+  prod: {
+    ENV_VARS: shared
+  }
+};
+
+module.exports = environments;
