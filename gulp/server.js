b0390d9b06aa10370cd1eb919e215243eb3221e6
Jurgis Bridzius <theghis@gmail.com>
2016-11-14 20:17:18 +0200

    New dependencies

--- gulp/server.js
+++ gulp/server.js
@@ -43,7 +43,7 @@ browserSync.use(browserSyncSpa({
   selector: '[ng-app]'// Only needed for angular apps
 }));
 
-gulp.task('serve', ['watch'], function () {
+gulp.task('serve', ['ng-config', 'watch'], function () {
   browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
 });
 
